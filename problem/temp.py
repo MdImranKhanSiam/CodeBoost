67f2de26403f210eaea38db8f387c959ae98b244
Imran Khan <mdimrankhansiam@gmail.com>
2026-04-13 03:38:21 +0600

    Problem Edit Option Added

--- problem/temp.py
+++ problem/temp.py
@@ -1,3 +1,146 @@
-a = int(input())
-b = int(input())
-print(a+b)
+{% extends 'home/master.html' %}
+{% block content %}
+{% load static %}
+
+<link rel="stylesheet" href="{% static 'css/problem/create_problem.css' %}" />
+
+
+<div class="create-problem-container">
+
+<form
+    {% if problem_type == "public" %}
+        action="{% url 'edit-problem' current_problem.id %}"
+    {% elif problem_type == "contest" %}
+        action="{% url 'edit-contest-problem' contest.id %}"
+    {% endif %}
+
+    method="POST">
+
+    {% csrf_token %}
+
+    <h2>Edit Problem</h2>
+
+    <div class="rules">
+        <h3>
+            Standard:
+        </h3>
+        <br>
+
+        <p>
+            * For testcases, every space & newline counts
+        </p>
+        
+
+    </div>
+
+    <br><br>
+
+    <input type="text" name="title" placeholder="Problem Title" value="{{ current_problem.title }}" required>
+
+    <textarea name="statement" placeholder="Problem Statement" required>{{ current_problem.statement }}</textarea>
+
+    <textarea name="problem_input" placeholder="Input Description" required>{{ current_problem.problem_input }}</textarea>
+
+    <textarea name="problem_output" placeholder="Output Description" required>{{ current_problem.problem_output }}</textarea>
+
+    <textarea name="note" placeholder="Note (optional)">{{ current_problem.note|default_if_none:'' }}</textarea>
+
+    <label for="difficulty">Difficulty:</label>
+    <select name="difficulty">
+        <option value="easy" {% if current_problem.difficulty == "easy" %}selected{% endif %}>Easy</option>
+        <option value="medium" {% if current_problem.difficulty == "medium" %}selected{% endif %}>Medium</option>
+        <option value="hard" {% if current_problem.difficulty == "hard" %}selected{% endif %}>Hard</option>
+        <option value="challenging" {% if current_problem.difficulty == "challenging" %}selected{% endif %}>Challenging</option>
+    </select>
+
+    <label for="time_limit">Time Limit</label>
+    <input type="number" name="time_limit" value="{{ current_problem.time_limit }}">
+
+    <label for="memory_limit">Memory Limit</label>
+    <input type="number" name="memory_limit" value="{{ current_problem.memory_limit }}">
+
+
+    <div class="divider"></div>
+
+    <h3 class="section-title">Test Cases</h3>
+
+    <div id="testcases-container">
+
+    </div>
+
+
+    <button type="button" class="btn btn-add" onclick="addTestcase()">
+        Add Test Case
+    </button>
+
+
+    <br>
+
+    <button type="submit" class="btn btn-submit">
+        Update Problem
+    </button>
+
+</form>
+
+</div>
+
+<script>
+    let current_testcases_list = [];
+
+    {% for testcase in current_testcases %}
+        current_testcases_list.push(
+            {
+                input_data: "{{ testcase.input_data|escapejs }}",
+                expected_output: "{{ testcase.expected_output|escapejs }}",
+                is_hidden: {{ testcase.is_hidden|yesno:"true,false" }},
+            }
+        );
+    {% endfor %}
+
+    let testCaseIndex = 0;
+
+    function addTestcase(tc = null) {
+        let input_data = "";
+        let expected_output = "";
+        let is_hidden = "checked";
+
+        if (tc) {
+            input_data = tc.input_data;
+            expected_output = tc.expected_output;
+            is_hidden = tc.is_hidden ? "checked" : "";
+        }
+
+        const container = document.getElementById("testcases-container")
+
+        const div = document.createElement("div")
+        div.classList.add("testcase")
+
+        div.innerHTML = `
+            <textarea name="testcase_input[]" placeholder="Input" required>${input_data}</textarea>
+
+            <textarea name="testcase_output[]" placeholder="Expected Output" required>${expected_output}</textarea>
+
+            <label>
+                <input type="checkbox" name="testcase_hidden[]" value="${testCaseIndex}" ${is_hidden}>
+                Hidden Test Case
+            </label>
+
+            <button type="button" class="btn btn-remove"
+            onclick="this.parentElement.remove()">
+            Remove
+            </button>
+        `
+
+        container.appendChild(div)
+
+        testCaseIndex++;
+    }
+
+    current_testcases_list.forEach(tc => {
+        addTestcase(tc);
+    });
+
+</script>
+
+
+{% endblock %}
--- problem/views.py
+++ problem/views.py
@@ -88,6 +88,8 @@ def create_problem(request):
         time_limit = request.POST.get('time_limit')
         memory_limit = request.POST.get('memory_limit')
 
+        testcases = json.loads(request.POST.get('testcases'))
+
         # All actions are discarded if the creation of either the problem or the testcase object fails.
         with transaction.atomic():
             problem = Problem.objects.create(
@@ -101,8 +103,6 @@ def create_problem(request):
                 memory_limit=memory_limit,
                 created_by=request.user
             )
-
-            testcases = json.loads(request.POST.get('testcases'))
 
             testcase_objects = []
 
@@ -129,6 +129,7 @@ def create_problem(request):
     return render(request, 'problem/create_problem.html', context)
 
 
+
 @permission_required('problem.change_problem', raise_exception=True)
 @login_required(login_url='/accounts/google/login/')
 def edit_problem(request, problem_id):
@@ -147,45 +148,38 @@ def edit_problem(request, problem_id):
         time_limit = request.POST.get('time_limit')
         memory_limit = request.POST.get('memory_limit')
 
-        testcase_inputs = request.POST.getlist('testcase_input[]')
-        testcase_outputs = request.POST.getlist('testcase_output[]')
-        testcases_hidden = request.POST.getlist('testcase_hidden[]')
+        testcases = json.loads(request.POST.get('testcases'))
 
         # All actions are discarded if the creation of either the problem or the testcase object fails.
         with transaction.atomic():
-            problem = Problem.objects.create(
-                title=title,
-                statement=statement,
-                problem_input=problem_input,
-                problem_output=problem_output,
-                note=note,
-                difficulty=difficulty,
-                time_limit=time_limit,
-                memory_limit=memory_limit,
-                created_by=request.user
-            )
+            current_problem.title=title
+            current_problem.statement=statement
+            current_problem.problem_input=problem_input
+            current_problem.problem_output=problem_output
+            current_problem.note=note
+            current_problem.difficulty=difficulty
+            current_problem.time_limit=time_limit
+            current_problem.memory_limit=memory_limit
+            current_problem.save()
 
-            testcases = []
+            current_problem.testcases.all().delete()
 
-            for i in range(len(testcase_inputs)):
-                hidden = False
+            testcase_objects = []
 
-                if str(i) in testcases_hidden:
-                    hidden = True
-
-                testcase = TestCase(
-                    problem=problem,
-                    input_data=testcase_inputs[i],
-                    expected_output=testcase_outputs[i],
-                    is_hidden=hidden
+            for testcase in testcases:
+                testcase_object = TestCase(
+                    problem=current_problem,
+                    input_data=testcase['testcase_input'],
+                    expected_output=testcase['testcase_output'],
+                    is_hidden=testcase['hidden_testcase']
                 )
 
-                testcases.append(testcase)
+                testcase_objects.append(testcase_object)
 
-            TestCase.objects.bulk_create(testcases)
+            TestCase.objects.bulk_create(testcase_objects)
 
-        if problem:
-            return redirect('problem-detail', problem.id)
+        if current_problem:
+            return redirect('problem-detail', current_problem.id)
         
 
     context = {
@@ -195,6 +189,7 @@ def edit_problem(request, problem_id):
     }
 
     return render(request, 'problem/edit_problem.html', context)
+
 
 
 @login_required(login_url='/accounts/google/login/')
