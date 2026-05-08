06707383289dddce5b45e55202a0199a94cc0b73
Imran Khan <mdimrankhansiam@gmail.com>
2026-05-08 23:28:11 +0600

    Update

--- problem/temp.py
+++
@@ -1,8 +0,0 @@
-row.innerHTML = `
-      <div>${p.id}</div>
-      <div>${p.title}</div>
-      <div class="difficulty ${p.difficulty}">${p.difficulty}</div>
-      <div class="status ${p.solved ? 'solved' : ''}">
-        ${p.solved ? '✔ Solved' : ''}
-      </div>
-    `;
