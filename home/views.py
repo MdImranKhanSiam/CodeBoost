e65356ca826eb4ff0e9d72cf45ee21ad36cca16f
Imran Khan <mdimrankhansiam@gmail.com>
2026-05-13 03:23:48 +0600

    Homepage Cached

--- home/views.py
+++ home/views.py
@@ -4,13 +4,22 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
 from . models import CodeSnippet
+from . cache import get_homepage, set_homepage, invalidate_homepage
 
 def home(request):
-    code_snippet = CodeSnippet.objects.get(title='Welcome to competitive programming')
+    context = get_homepage()
+
+    if not context:
+        code_snippet = CodeSnippet.objects.get(title='Welcome to competitive programming')
     
-    context = {
-        'code_snippet' : code_snippet,
-    }
+        context = {
+            'code_snippet' : {
+                'title': code_snippet.title,
+                'code': code_snippet.code
+            },
+        }
+
+        set_homepage(context)
 
     return render(request, 'home/home.html', context)
 
